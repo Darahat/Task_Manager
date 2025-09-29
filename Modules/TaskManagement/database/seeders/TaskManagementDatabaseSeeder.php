6242aa172f36f708ca0d8f754af688f4025c7c5b
Didarul Alam Rahat <darahat42@gmail.com>
2025-09-29 15:43:31 +0600

    Feat:Task Manager  initialize

---
+++ Modules/TaskManagement/database/seeders/TaskManagementDatabaseSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\TaskManagement\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class TaskManagementDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // $this->call([]);
+    }
+}
